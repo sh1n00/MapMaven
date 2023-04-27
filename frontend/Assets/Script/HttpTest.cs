bba9f39a5b6766ef01f4a1cbebf5229e8cf2efb0
sh1n00 <rinjyou0031@toki.waseda.jp>
2023-04-28 03:59:52 +0900

    add: front-backの通信の実装

--- frontend/Assets/Script/HttpTest.cs
+++ frontend/Assets/Script/HttpTest.cs
@@ -12,22 +12,48 @@ public class HttpTest : MonoBehaviour
 
     async void Start()
     {
-        var test = await TextToAudio("1", "true", "Hello");
-        byte[] data = System.Text.Encoding.UTF8.GetBytes(test);
-        float[] samples = new float[data.Length / 4]; //size of a float is 4 bytes
-        Buffer.BlockCopy(data, 0, samples, 0, data.Length);
-     
-        int channels = 1; //Assuming audio is mono because microphone input usually is
-        int sampleRate = 27000; //Assuming your samplerate is 44100 or change to 48000 or whatever is appropriate
-        Debug.Log(sampleRate);
-        AudioClip clip = AudioClip.Create("ClipName", samples.Length, channels, sampleRate, false);
-        clip.SetData(samples, 0);
-        //
-        // // Play the AudioClip
-        AudioSource.PlayClipAtPoint(clip, transform.position);
+        AudioSource audioSource = GetComponent<AudioSource>();
+        var test1 = await GuideByText("サイバネティクスの場所はどこですか?");
+        var test = await TextToAudio("1", "true", test1);
+        byte[] binaryData = Convert.FromBase64String(test);
+        AudioClip clip = Wav.ToAudioClip(binaryData, "test");
+        audioSource.PlayOneShot(clip);
     }
 
-    private async Task<String> TextToAudio(string speaker, string enable_interrogative_upspeak, string text)
+    private async Task<string> GuideByText(string text)
+    {
+        var url = "http://localhost:8080/guide-by-text";
+        var query = new Dictionary<String, String>
+        {
+            { "text", text },
+        };
+        var queryString = System.Web.HttpUtility.ParseQueryString("");
+     
+        foreach (KeyValuePair<String, String> pair in query)
+        {
+            queryString.Add(pair.Key, pair.Value);   
+        }
+       
+        var uriBuilder = new UriBuilder(url)
+        {
+            Query = queryString.ToString()
+        };
+        
+        try
+        {
+            var response = await _httpClient.GetAsync(uriBuilder.Uri);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            ChatCompletionResponse json = JsonUtility.FromJson<ChatCompletionResponse>(responseBody);
+            return json.choices[0].message.content;
+        }
+        catch (HttpRequestException e)
+        {
+            Debug.Log($"HTTP request failed: {e}");
+            return "text";
+        }
+    }
+
+    private async Task<string> TextToAudio(string speaker, string enable_interrogative_upspeak, string text)
     {
         var url = "http://localhost:8080/text-to-audio";
         var query = new Dictionary<String, String>
@@ -52,14 +78,13 @@ public class HttpTest : MonoBehaviour
         {
             var response = await _httpClient.GetAsync(uriBuilder.Uri);
             var responseBody = await response.Content.ReadAsStringAsync();
-            Debug.Log(responseBody);
             AudioResponse json = JsonUtility.FromJson<AudioResponse>(responseBody);
-            return json.AudioBinary;
+            return json.audio_binary;
         }
         catch (HttpRequestException e)
         {
             Debug.Log($"HTTP request failed: {e}");
-            return "Error";
+            return "text";
         }
     }
     
