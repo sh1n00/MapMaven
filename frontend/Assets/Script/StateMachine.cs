5236d4703fc392f8bc6d6f7c6d8ab780a6b145fd
Hiroyuki Ishizuka <56533309+yuk4869@users.noreply.github.com>
2023-04-29 00:55:31 +0900

    add thinking time

--- frontend/Assets/Script/StateMachine.cs
+++ frontend/Assets/Script/StateMachine.cs
@@ -2,12 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static GoogleCloudStreamingSpeechToText.StreamingRecord;
+using GoogleCloudStreamingSpeechToText;
 
 
 public class StateMachine : MonoBehaviour
 {
     public AudioSource audioSource;
+    public AudioClip thinkingClip;
+    private bool isAudioPlay;
+    public StreamingRecord streamingRecord;
+    private bool isStandBy = false;
+    private bool isAwaitReply = false;
     private enum gameState
     {
         standby,
@@ -20,32 +25,49 @@ public class StateMachine : MonoBehaviour
 
     private void LateUpdate()
     {
+        //Debug.Log(_currentState);
         switch(_currentState)
         {
             case gameState.standby://not user 
-                
-                
+                if(!isStandBy)streamingRecord.StartListening();
+                isStandBy = true;
+                isAudioPlay = false;
                 break;
             case gameState.recognition://user talk start
-
+                isStandBy = false;
                 break;
             case gameState.awaitReply://user talk end and chatgpt
+                if (!isStandBy) streamingRecord.StartListening();
+                if (!isAwaitReply)
+                {
+                    audioSource.PlayOneShot(thinkingClip);
+                }
+                isAwaitReply = true;
 
                 break;
             case gameState.reply://avater talk
-
+                isStandBy = false;
+                isAwaitReply = false;
+                if (!audioSource.isPlaying && isAudioPlay)
+                {
+                    _currentState = gameState.standby;
+                    
+                }
                 break;
         }
     }
     public async void EndRecognition(string finalDetection)
     {
         _currentState = gameState.awaitReply;
+        streamingRecord.StopListening();
         var text = await HttpTest.GuideByText(finalDetection);
         Debug.Log(text);
-        var test = await HttpTest.TextToAudio("1", "true", text);
+        var test = await HttpTest.TextToAudio("2", "true", text);
         byte[] binaryData = Convert.FromBase64String(test);
         AudioClip clip = Wav.ToAudioClip(binaryData, "test");
         audioSource.PlayOneShot(clip);
+        _currentState = gameState.reply;
+        isAudioPlay = true;
     }
     public void StartRecognition()
     {
